64eb99e11d3e30d80f2490bf95ef9f9c9051fff7
Milomir Dragović <hello@milomir.rs>
2021-03-08 20:18:47 +0100

    add class Car and few example usages

---
+++ src/main/java/main/car/Main.java
@@ -0,0 +1,27 @@
+package main.car;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 6;
+        Car audi = new Car();
+        audi.brand = "Audi";
+        audi.model = "A4";
+        audi.color = "black";
+        audi.printAttributes();
+        
+        Car bmw = new Car();
+        bmw.brand = "BMW";
+        bmw.model = "X5";
+        bmw.printAttributes();
+        
+        Car fiat = new Car(2010);
+        System.out.println("Godina proizvodnje: " + fiat.getBuildYear());
+        
+        Car lada = new Car("Lada", "Niva", "green", 2018);
+        lada.printAttributes();
+        
+    }
+
+}
